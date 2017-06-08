6649e2b1429e626b066166362c62cb4e3b3783f4
Petya Naumova <petya.naumova@mentormate.com>
2017-06-08 19:52:02 +0300

    save work - css and html changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
