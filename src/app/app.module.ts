43009a77f00f348e64ec9e43639a5c1adc9f1b0f
Petya Naumova <petya.naumova@mentormate.com>
2017-06-09 13:11:41 +0300

    add angular-2-local-storage npm module and change all css files to scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import { LocalStorageModule } from 'angular-2-local-storage';
+
+// Components
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AddRecipesComponent } from './add-recipes/add-recipes.component';
 import { SearchRecipesComponent } from './search-recipes/search-recipes.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +19,11 @@ import { SearchRecipesComponent } from './search-recipes/search-recipes.componen
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LocalStorageModule.withConfig({
+            prefix: 'my-dream-app',
+            storageType: 'localStorage'
+        })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
   moduleId: module.id,
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.scss']
 })
 
 export class DashboardComponent {
