46067cb507d157132353ddb9b3c050e960f0e18d
Petya Naumova <petianaumova@gmail.com>
2017-06-16 01:06:32 +0300

    set path for the ingredient template

--- src/app/add-recipes/ingredient/ingredient.component.ts
+++ src/app/add-recipes/ingredient/ingredient.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-add-ingredient',
-    templateUrl: './ingredient/ingredient.component.html'
+    templateUrl: './ingredient.component.html'
 })
 
 export class IngredientComponent {
