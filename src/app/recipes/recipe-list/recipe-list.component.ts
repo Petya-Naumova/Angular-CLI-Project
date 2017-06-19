a7325e8a42770674c73d59f776e04f5bd652b7a6
Petya Naumova <petya.naumova@mentormate.com>
2017-06-19 20:08:11 +0300

    save current work - css improvements

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,15 +9,15 @@ export class RecipeListComponent implements OnInit {
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe [] = [
-    new Recipe('Lemon Cake', 'Lemon Cake',
+    new Recipe('Lemon Cake', 'Cupcake ipsum dolor sit amet jelly apple pie sesame snaps biscuit.',
     '/../../assets/images/lemon-cake.jpg'),
     new Recipe('Pancakes', 'Try these marvelous pancakes, very easy to make',
     '/../../assets/images/pancakes.jpg'),
-    new Recipe('Sandwiches', 'Sandwiches',
+    new Recipe('Sandwiches', 'Sweet roll soufflé cookie marshmallow cake oat cake toffee bonbon.',
     '/../../assets/images/sandwiches.jpg'),
     new Recipe('Cake', 'This is how you can make very tasty cake only with...',
     '/../../assets/images/cake.jpg'),
-    new Recipe('Pizza', 'Pizza',
+    new Recipe('Pizza', 'Candy donut pudding. Cotton candy jujubes oat cake tiramisu powder dessert.',
     '/../../assets/images/pizza.jpg'),
     new Recipe('Chocolate Cake', 'Chocolate Cake',
     '/../../assets/images/chocolate-cake.jpg'),
