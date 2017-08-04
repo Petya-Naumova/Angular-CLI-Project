041d10bc0bd68cbabe7e238bdd7593ee9c5e5cdc
Petya Naumova <petya.naumova@mentormate.com>
2017-08-04 16:06:22 +0300

    add synchronization between edit and recipe-item componentS

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { Recipe } from '../recipe.model';
+import { RecipeService } from './../recipe.service';
+
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.scss']
 })
 export class RecipeEditComponent implements OnInit {
-
+  recipe: Recipe;
   id: number;
   editMode = false;
 
@@ -16,19 +19,24 @@ export class RecipeEditComponent implements OnInit {
   post: any;
   description: '';
   name: '';
+  image: '';
 
-  constructor(private route: ActivatedRoute, private fb: FormBuilder) {
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private fb: FormBuilder) {
     this.rForm = fb.group({
       'name': [null, Validators.required],
+      'image': [null, Validators.required],
       'description': [null, Validators.compose([Validators.required, Validators.maxLength(200)])],
-      'validate': ''
     });
   }
 
   editForm(post) {
-    console.log(post);
+    console.log(this.recipe);
+    this.recipe.name = post.name;
+    this.recipe.imageRecipePath = post.image;
+    this.recipe.description = post.description;
     this.name = post.name;
     this.description = post.description;
+    this.image = post.image;
   }
 
   ngOnInit() {
@@ -37,6 +45,7 @@ export class RecipeEditComponent implements OnInit {
         (params: Params) => {
           this.id = +params['id'];
           this.editMode = params['id'] != null;
+          this.recipe = this.recipeService.getRecipeByIndex(this.id);
         }
       );
   }
